21f26ef944524eeffba684382c14756744977256
Jonathan Gawrych <Jonathan.Gawrych@gmail.com>
2018-07-18 15:39:08 -0700

    Allow brute forcing empty commits

--- gitbrute.go
+++ gitbrute.go
@@ -78,7 +78,7 @@ func main() {
 	w := <-winner
 	close(done)
 
-	cmd := exec.Command("git", "commit", "--amend", "--date="+w.author.String(), "--file=-")
+	cmd := exec.Command("git", "commit", "--allow-empty", "--amend", "--date="+w.author.String(), "--file=-")
 	cmd.Env = append([]string{"GIT_COMMITTER_DATE=" + w.committer.String()}, os.Environ()...)
 	cmd.Stdout = os.Stdout
 	cmd.Stdin = bytes.NewReader(msg)
